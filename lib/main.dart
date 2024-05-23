97b3c7d8187c17e6f14872da5261a9dd30b42ba6
Amgad Elmarghny <amgadsaad22@gmail.com>
2024-05-24 00:09:06 +0300

    create on boarding view

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:social_media_app/modules/on_boarding/on_boarding_view.dart';
 
 void main() {
   runApp(const SocialMediaApp());
@@ -6,9 +7,14 @@ void main() {
 
 class SocialMediaApp extends StatelessWidget {
   const SocialMediaApp({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return MaterialApp(
+      initialRoute: OnBoardingView.routeViewName,
+      routes: {
+        OnBoardingView.routeViewName: (context) => const OnBoardingView(),
+      },
+    );
   }
 }
