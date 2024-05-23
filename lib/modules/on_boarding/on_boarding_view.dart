6b59a423578274ad546905debe8828939cd8ea38
Amgad Elmarghny <amgadsaad22@gmail.com>
2024-05-24 00:22:50 +0300

    create on boarding view body

--- lib/modules/on_boarding/on_boarding_view.dart
+++ lib/modules/on_boarding/on_boarding_view.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:social_media_app/modules/on_boarding/on_bording_view_body.dart';
 
 class OnBoardingView extends StatelessWidget {
   const OnBoardingView({super.key});
   static const String routeViewName = "onBoardingView";
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: OnBoardingViewBody(),
+    );
   }
 }
