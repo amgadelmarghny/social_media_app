a3ffa6bb1d0f0da455a4db0f5bc5605523da901b
Amgad Elmarghny <amgadsaad22@gmail.com>
2024-05-24 00:02:34 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const SocialMediaApp());
+}
+
+class SocialMediaApp extends StatelessWidget {
+  const SocialMediaApp({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
