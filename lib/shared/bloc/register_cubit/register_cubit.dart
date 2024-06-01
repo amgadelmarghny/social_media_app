26ca457887233d941dc2cca07c38c43640e57dcc
Amgad Elmarghny <amgadsaad22@gmail.com>
2024-06-01 23:14:02 +0300

    add user register to register cubit

--- lib/shared/bloc/register_cubit/register_cubit.dart
+++ lib/shared/bloc/register_cubit/register_cubit.dart
@@ -1,8 +1,21 @@
 import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 part 'register_state.dart';
 
 class RegisterCubit extends Cubit<RegisterState> {
   RegisterCubit() : super(RegisterInitial());
+
+  UserCredential? userCredential;
+
+  void userRegister({required String email, required String password}) async {
+    emit(RegisterLoadingState());
+    try {
+      userCredential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email, password: password);
+      emit(RegisterLoadingState());
+    } on Exception catch (error) {
+      emit(RegisterFailureState(errMessage: error.toString()));
+    }
+  }
 }
--- lib/shared/bloc/register_cubit/register_state.dart
+++ lib/shared/bloc/register_cubit/register_state.dart
@@ -1,6 +1,15 @@
 part of 'register_cubit.dart';
 
-@immutable
 sealed class RegisterState {}
 
 final class RegisterInitial extends RegisterState {}
+
+final class RegisterLoadingState extends RegisterState {}
+
+final class RegisterSuccessState extends RegisterState {}
+
+final class RegisterFailureState extends RegisterState {
+  final String errMessage;
+
+  RegisterFailureState({required this.errMessage});
+}
