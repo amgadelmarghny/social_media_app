93ce01da5f7c6987a0741388ef0d1012ba3f63f1
Amgad Elmarghny <amgadsaad22@gmail.com>
2024-06-04 02:29:54 +0300

    create user model and add cloud fire

---
+++ lib/models/onBoarding/user_model.dart
@@ -0,0 +1,43 @@
+class UserModel {
+  final String uid;
+  final String firstName;
+  final String lastName;
+  final String email;
+  final String dateAndMonth;
+  final String year;
+  final String gender;
+
+  UserModel({
+    required this.uid,
+    required this.firstName,
+    required this.lastName,
+    required this.email,
+    required this.dateAndMonth,
+    required this.year,
+    required this.gender,
+  });
+
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(
+      uid: json['uid'],
+      firstName: json['firstName'],
+      lastName: json['lastName'],
+      email: json['email'],
+      dateAndMonth: json['dateAndMonth'],
+      year: json['year'],
+      gender: json['gender'],
+    );
+  }
+
+  Map<String, dynamic> tojson() {
+    return {
+      'uid': uid,
+      'firstName': firstName,
+      'lastName': lastName,
+      'email': email,
+      'dateAndMonth': dateAndMonth,
+      'year': year,
+      'gender': gender,
+    };
+  }
+}
