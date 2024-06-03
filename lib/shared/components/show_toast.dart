f0c5427aab12ecdc56bc239a6528416ee192f001
Amgad Elmarghny <amgadsaad22@gmail.com>
2024-06-04 02:00:59 +0300

    create show toast function

---
+++ lib/shared/components/show_toast.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+void showToast({required String msg, required ToastState toastState}) {
+  Fluttertoast.showToast(
+      msg: msg,
+      gravity: ToastGravity.TOP,
+      toastLength: Toast.LENGTH_SHORT,
+      timeInSecForIosWeb: 1,
+      backgroundColor: toastColor(toastState),
+      textColor: Colors.white,
+      fontSize: 16.0);
+}
+
+enum ToastState { error, worrning, success }
+
+Color toastColor(ToastState toastState) {
+  late Color color;
+  switch (toastState) {
+    case ToastState.success:
+      color = Colors.green;
+      break;
+    case ToastState.worrning:
+      color = Colors.yellow;
+      break;
+    case ToastState.error:
+      color = Colors.red;
+  }
+  return color;
+}
