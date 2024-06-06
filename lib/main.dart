3c6598f0e87acc096a9debd0502d54cd84230523
Amgad Elmarghny <amgadsaad22@gmail.com>
2024-06-06 21:10:58 +0300

    create cache helper

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,12 @@ import 'package:social_media_app/modules/on_boarding/on_boarding_view.dart';
 import 'package:social_media_app/modules/reqister/reqister_view.dart';
 import 'package:social_media_app/shared/bloc/app_cubit/app_cubit.dart';
 import 'package:social_media_app/shared/bloc/bloc_observer.dart';
+import 'package:social_media_app/shared/bloc/network/local/cache_helper.dart';
 
 void main() async {
   Bloc.observer = MyBlocObserver();
   WidgetsFlutterBinding.ensureInitialized();
+  await CacheHelper.init();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
