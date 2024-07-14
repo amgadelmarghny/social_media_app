0c62a54eba0cb5f1895261058baaf2be4766f667
Amgad Elmarghny <amgadsaad22@gmail.com>
2024-07-14 03:46:12 +0300

    finsh edit user birthday section ui

--- lib/modules/edit_profile/widgets/edit_user_birthday_section.dart
+++ lib/modules/edit_profile/widgets/edit_user_birthday_section.dart
@@ -1,17 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:icon_broken/icon_broken.dart';
 
-class EditUserBirthdaySection extends StatelessWidget {
+import '../../../shared/components/expanded_list_animation_widget.dart';
+import '../../../shared/components/text_form_field.dart';
+
+class EditUserBirthdaySection extends StatefulWidget {
   const EditUserBirthdaySection({
     super.key,
   });
 
   @override
+  State<EditUserBirthdaySection> createState() => _EditUserBirthdaySectionState();
+}
+
+class _EditUserBirthdaySectionState extends State<EditUserBirthdaySection> {
+  bool isStretchedDropDown = false;
+  @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: const Text('Birthday'),
-      trailing: const Icon(IconBroken.Arrow___Down_2),
-      onTap: () {},
+    return Column(
+      children: [
+        ListTile(
+          title: const Text('BirthDay'),
+          trailing: isStretchedDropDown
+              ? const Icon(IconBroken.Arrow___Up_2)
+              : const Icon(IconBroken.Arrow___Down_2),
+          onTap: () {
+            setState(() {
+              isStretchedDropDown = !isStretchedDropDown;
+            });
+          },
+        ),
+        ExpandedSection(
+          expand: isStretchedDropDown,
+          height: 100,
+          child: const Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              CustomTextField(
+                outLineBorderColor: Colors.grey,
+                contentVerticalPadding: 10,
+                textInputType: TextInputType.datetime,
+                hintText: 'DD/MM/YYYY',
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
