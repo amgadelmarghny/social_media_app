c28f8ecede2174b0de82b82491b96ba514f43b81
Amgad Elmarghny <amgadsaad22@gmail.com>
2024-07-29 18:02:28 +0300

    finsh comment sheet logic

--- lib/modules/feeds/widgets/comments_sheet.dart
+++ lib/modules/feeds/widgets/comments_sheet.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:social_media_app/shared/style/theme/constant.dart';
 import 'package:social_media_app/shared/style/theme/theme.dart';
 import '../../../shared/components/comment_item.dart';
+import '../../../shared/style/fonts/font_style.dart';
 
 class CommentsSheet extends StatelessWidget {
   const CommentsSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
+    TextEditingController commentController = TextEditingController();
     return Container(
       decoration: themeColor(),
       padding: const EdgeInsets.only(top: 40),
@@ -20,6 +23,24 @@ class CommentsSheet extends StatelessWidget {
                 child: ListView.builder(
                   itemBuilder: (context, index) => const CommentItem(),
                   itemCount: 20,
+                ),
+              ),
+              TextField(
+                style: FontsStyle.font18Popin(isShadow: true),
+                controller: commentController,
+                decoration: InputDecoration(
+                  hintStyle: FontsStyle.font18Popin(isShadow: true),
+                  suffixIconColor: defaultColor,
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      commentController.clear();
+                    },
+                    icon: const Icon(Icons.send_outlined),
+                  ),
+                  hintText: 'Write comment...',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
                 ),
               )
             ],
