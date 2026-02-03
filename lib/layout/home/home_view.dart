6215a55ed7037ed39ee85f3b13d81e5d1c1e43dd
Amgad Elmarghny <amgadsaad22@gmail.com>
2026-02-03 21:12:51 +0200

    add upgrade alert

--- lib/layout/home/home_view.dart
+++ lib/layout/home/home_view.dart
@@ -5,6 +5,7 @@ import 'package:social_media_app/layout/home/components/custom_bottom_nav_bar.da
 import 'package:social_media_app/shared/bloc/social_cubit/social_cubit.dart';
 import 'package:social_media_app/shared/components/show_toast.dart';
 import 'package:social_media_app/shared/style/theme/theme.dart';
+import 'package:upgrader/upgrader.dart';
 
 /// The main HomeView widget, which is a StatefulWidget.
 /// This is the entry point for the home screen of the app.
@@ -43,33 +44,39 @@ class _HomeViewState extends State<HomeView> {
         children: [
           // Main content area of the app.
           Scaffold(
-            body: BlocConsumer<SocialCubit, SocialState>(
-              // Listen for error states and show toasts if needed.
-              listener: (context, state) {
-                if (state is GetMyDataFailureState) {
-                  // Show an error toast if user data fails to load.
-                  showToast(
-                      msg: state.errMessage, toastState: ToastState.error);
-                }
-                if (state is GetFeedsPostsFailureState) {
-                  // Show an error toast if feed posts fail to load.
-                  showToast(
-                      msg: state.errMessage, toastState: ToastState.error);
-                }
-              },
-              builder: (context, state) {
-                // If user data failed to load, show an error message in the center of the screen.
-                if (state is GetMyDataFailureState) {
-                  return const Center(
-                    child: Text('Something went wrong'),
-                  );
-                }
-                // Only show the main content if user data and posts are loaded.
-                // The currentBody is selected based on the currentBottomNavBarIndex from the SocialCubit.
-                return BlocProvider.of<SocialCubit>(context).currentBody[
-                    BlocProvider.of<SocialCubit>(context)
-                        .currentBottomNavBarIndex];
-              },
+            body: UpgradeAlert(
+              dialogStyle: UpgradeDialogStyle.material,
+              upgrader: Upgrader(
+                durationUntilAlertAgain: const Duration(days: 1),
+              ),
+              child: BlocConsumer<SocialCubit, SocialState>(
+                // Listen for error states and show toasts if needed.
+                listener: (context, state) {
+                  if (state is GetMyDataFailureState) {
+                    // Show an error toast if user data fails to load.
+                    showToast(
+                        msg: state.errMessage, toastState: ToastState.error);
+                  }
+                  if (state is GetFeedsPostsFailureState) {
+                    // Show an error toast if feed posts fail to load.
+                    showToast(
+                        msg: state.errMessage, toastState: ToastState.error);
+                  }
+                },
+                builder: (context, state) {
+                  // If user data failed to load, show an error message in the center of the screen.
+                  if (state is GetMyDataFailureState) {
+                    return const Center(
+                      child: Text('Something went wrong'),
+                    );
+                  }
+                  // Only show the main content if user data and posts are loaded.
+                  // The currentBody is selected based on the currentBottomNavBarIndex from the SocialCubit.
+                  return BlocProvider.of<SocialCubit>(context).currentBody[
+                      BlocProvider.of<SocialCubit>(context)
+                          .currentBottomNavBarIndex];
+                },
+              ),
             ),
           ),
           // The custom bottom navigation bar, always aligned to the bottom of the screen.
